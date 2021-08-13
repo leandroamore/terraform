9aafe48483ea3991896dcdc914f3467185d104f5
Leandro Amore <leandro.amore@prisma.cc>
2021-08-13 09:29:25 -0300

    agrego datasource

---
+++ data_source.tf
@@ -0,0 +1,5 @@
+data "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = var.acr_rg_name
+}
+
---
+++ var-datasource.tf
@@ -0,0 +1,9 @@
+variable "acr_name" {
+    type        = string
+    default = "vupaas"
+    }
+    
+variable "acr_rg_name" {
+    type        = string
+    default = "RSG_ACRPaaS"
+    }
